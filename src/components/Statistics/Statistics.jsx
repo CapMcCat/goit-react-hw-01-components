df5122faffbbebd29af576e072c4caa60093a650
Kateryna Burlaka <71180234+CapMcCat@users.noreply.github.com>
2021-08-15 20:56:18 +0300

    add Statistics markup

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+const Statistics = ({ stats, title }) => {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>Upload stats</h2>}
+
+      <ul className={s.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li key={id} className={s.item}>
+            <span className={s.label}>.{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }),
+};
+
+export default Statistics;
