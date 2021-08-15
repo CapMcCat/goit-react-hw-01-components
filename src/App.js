659ea05bd1da81400bdffcd13bbd93c6e036fb2d
Kateryna Burlaka <71180234+CapMcCat@users.noreply.github.com>
2021-08-15 13:06:19 +0300

    Profile markup done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Profile from './components/Profile/Profile';
+import user from './user.json';
+
+function App() {
+  return (
+    <div >
+      <Profile
+    name={user.name}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import defaultImage from './default.jpg';
+import s from './Profile.module.css'
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+    return (<div className={s.profile}>
+        <div className={s.description}>
+            <img
+                src={avatar}
+                alt="Аватар пользователя"
+                className={s.avatar}
+            />
+            <p className={s.name}>{name}</p>
+            <p className={s.tag}>@{tag}</p>
+            <p className={s.location}>{location}</p>
+        </div>
+
+        <ul className={s.stats}>
+            <li key='followers'>
+                <span className={s.label}>Followers</span>
+                <span className={s.quantity}>{stats.followers}</span>
+            </li>
+            <li key='views'>
+                <span className={s.label}>Views</span>
+                <span className={s.quantity}>{stats.views}</span>
+            </li>
+            <li key='likes'>
+                <span className={s.label}>Likes</span>
+                <span className={s.quantity}>{stats.likes}</span>
+            </li>
+        </ul>
+    </div>);
+};
+
+Profile.defaultProps = {
+    avatar: defaultImage,
+}
+
+Profile.propTypes = {
+    name: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.objectOf(PropTypes.number).isRequired,
+
+}
+
+export default Profile;
