8fa4f096efcf5724e04ef69dfa2ede7f6f7534f9
Kateryna Burlaka <71180234+CapMcCat@users.noreply.github.com>
2021-08-16 13:13:14 +0300

    add transaction markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Profile from './components/Profile/Profile';
-import user from './user.json';
+import user from './components/Profile/user.json';
 import Statistics from './components/Statistics/Statistics';
-import statisticalData from './statistical-data.json';
+import statisticalData from './components/Statistics/statistical-data.json';
 import FriendList from './components/FriendList/FriendList';
-import friends from './friends.json';
+import friends from './components/FriendList/friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/ transactions.json';
 
 function App() {
   return (
@@ -17,7 +19,8 @@ function App() {
       />
       <Statistics title='Upload stats' stats={statisticalData} />;
       <Statistics stats={statisticalData} />;
-      <FriendList friends={friends} />,
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={s.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.defaultProps = {
+  amount: '--',
+  currency: '--',
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
+export default TransactionHistory;
